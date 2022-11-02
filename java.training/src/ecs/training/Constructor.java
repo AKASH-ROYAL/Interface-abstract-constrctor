7a97a0b09074f1386b064efc71aee57f52d18894
AKASH-ROYAL <115221061+AKASH-ROYAL@users.noreply.github.com>
2022-11-01 21:09:13 -0700

    passing arguments

--- java.training/src/ecs/training/Constructor.java
+++ java.training/src/ecs/training/Constructor.java
@@ -3,6 +3,7 @@ package ecs.training;
 interface Nickname{
 String nickname=" Code wizard";
 void nick();
+void name();
 }
 
 abstract class LuckNo{
@@ -11,7 +12,7 @@ abstract void luckno();
 }
 
 class  SimpleName extends LuckNo implements Nickname {
-void name()
+public void name()
 {
 	String str= "Akash" ;
 	System.out.println( str);
@@ -25,6 +26,31 @@ public void luckno(){
 }
 }
 
+class praveen implements Nickname{
+	
+	public void name()
+	{
+		String str= "Praveen" ;
+		System.out.println( str);
+
+		}
+	public void nick() { 
+		System.out.println("NICK NAME ="+ nickname+" on JAVA");
+	}
+	public void luckno(){
+		int lno=10;
+		System.out.println("mMy lucky no is " + lno);
+	}
+}
+//class argument {
+//	
+//	void argmethod(Nickname n) {
+//	System.out.println("This is method that how we have to pass arguments")	;
+//	n.name();
+//	n.nick();
+//	}
+//}
+
 
 class Construct extends SimpleName {
  Construct(){
@@ -32,11 +58,28 @@ class Construct extends SimpleName {
 		nick();
 		luckno();
 }}
-
+class Construct1 extends praveen
+{
+	Construct1(){
+	name();
+	nick();
+	luckno();
+}
+}
  
 public class Constructor{
 public static void main(String[] args) 
 {
 Construct obect=new Construct();
+Construct1 obect1=new Construct1();
+praveen p=  new praveen();
+
+//argument arg =new argument();
+//
+//
+//arg.argmethod(obect);
+//arg.argmethod(p);
+
+
 }
 }
